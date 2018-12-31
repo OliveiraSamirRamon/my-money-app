6308f98a0180f20b19fa81b5a3697e6b0d8ae318
Samir Ramon de Oliveira <samir.ramon@gmail.com>
2018-12-31 00:30:28 -0200

    Seção 8, aula 94

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -7,3 +7,10 @@ mongoose.Promise = global.Promise
 
 //Exporta a conexão do mongodb
 module.exports = mongoose.connect('mongodb://localhost/mymoney')
+
+//muda a mensagem de erro
+//'{PATH}' é o nome do atributo
+mongoose.Error.messages.general.required ="O atributo '{PATH}' é obrigatório."
+mongoose.Error.messages.Number.min ="O '{VALUE}' informado é menor que o limite mínimo de '{MIN}'."
+mongoose.Error.messages.Number.max ="O '{VALUE}' informado é maior que o limite máximo de '{MAX}'."
+mongoose.Error.messages.String.enum ="'{VALUE}' não é válido para o atributo '{PATH}'."
